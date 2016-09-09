e2e6db446b5ea09abcf629e48fe61308b554febe
Ketul Patel <kjpatel2010@gmail.com>
2016-09-09 10:17:35 -0700

    Initial Commit

--- CoreMotionStarter/ViewController.swift
+++ CoreMotionStarter/ViewController.swift
@@ -7,19 +7,29 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    // step count outlet
+    
+    @IBOutlet weak var stepCount: UILabel!
+    // reset button
+    @IBAction func resetButtonPressed(sender: UIButton) {
+        pedometer.stopPedometerUpdates()
+        stepCount.text = "0"
+    }
+    // start button
+    @IBAction func startButtonPressed(sender: UIButton) {
+        pedometer.startPedometerUpdatesFromDate(NSDate(), withHandler: { data, error in
+            print("Update \(data.numberOfSteps)")
+            dispatch_async(dispatch_get_main_queue()) {
+                self.stepCount.text = String(stringInterpolationSegment: data.numberOfSteps)
+            }
+        })
+    }
+    let pedometer: CMPedometer = CMPedometer()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
